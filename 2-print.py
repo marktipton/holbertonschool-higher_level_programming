93f7c6b08224be8a62c3dd40f61be25411b365d8
marktipton <6675@holbertonstudent.com>
2023-09-05 11:17:59 -0700

    added new line

--- 2-print.py
+++ 2-print.py
@@ -3,4 +3,4 @@
 #StringVar = '"Programming is like building a multilingual puzzle'
 #print(StringVar)
 
-print('"Programming is like building a multilingual puzzle')
+print('"Programming is like building a multilingual puzzle\n')
