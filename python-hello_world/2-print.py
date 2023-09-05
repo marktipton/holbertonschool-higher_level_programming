b10892df272d9719bca5559214aba398a9e0f063
marktipton <6675@holbertonstudent.com>
2023-09-05 11:35:29 -0700

    removed new line and comments

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-
-#StringVar = '"Programming is like building a multilingual puzzle'
-#print(StringVar)
-
-print('"Programming is like building a multilingual puzzle\n')
+print('"Programming is like building a multilingual puzzle')
