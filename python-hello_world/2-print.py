7f1dcca1349d93fa272cb67676aa2458ca31c9fe
marktipton <6675@holbertonstudent.com>
2023-09-05 11:34:00 -0700

    made directory and moved files (oops)

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#StringVar = '"Programming is like building a multilingual puzzle'
+#print(StringVar)
+
+print('"Programming is like building a multilingual puzzle\n')
---
+++ python-hello_world/README.md
@@ -0,0 +1,2 @@
+# holbertonschool-higher_level_programming
+repository for Python and other higher level languages.
