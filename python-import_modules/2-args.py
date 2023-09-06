273d1da1dbf04e6de0f5ffd7699a123554269031
marktipton <6675@holbertonstudent.com>
2023-09-06 15:29:30 -0700

    from sys import argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+from sys import argv
+if __name__ == "__main__":
+    n_args = len(argv) - 1
+    print(f"{n_args}", end=" ")
+    if n_args == 1:
+        print("argument:")
+    elif n_args == 0:
+        print("arguments.")
+    else:
+        print("arguments:")
+    for n in range(1, n_args + 1):
+        print("{:d}: {}".format(n, argv[n]))
