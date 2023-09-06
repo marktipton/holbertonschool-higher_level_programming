51200f8a88f243cd166d8467a8fc03543a57a1e4
marktipton <6675@holbertonstudent.com>
2023-09-06 11:08:23 -0700

    can use += to add things to a string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def uppercase(string):
+    result = ""
+    for char in string:
+        if 'a' <= char <= 'z':
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+            result += "{}".format(upper_char)
+        else:
+            result += "{}".format(char)
+
+    print(result)
