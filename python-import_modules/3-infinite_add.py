16c2a9917fdb393fe612d712763431316bfaee25
marktipton <6675@holbertonstudent.com>
2023-09-06 15:42:59 -0700

    total += argv[n]

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    total = 0
+    n_args = len(argv)
+    for n in range(1, n_args):
+        total += int(argv[n])
+
+    print(f"{total}")
