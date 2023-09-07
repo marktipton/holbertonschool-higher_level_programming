f812ba81e66c5d2f10edb9c6cbb8e2a3ed3ef5eb
marktipton <6675@holbertonstudent.com>
2023-09-07 09:20:14 -0700

    changed if statements

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@ import hidden_4
 
 if __name__ == "__main__":
     for str in dir(hidden_4):
-        if str[:2] != "__":
+        if str[0] != "_" and str[1] != "_":
             print("{}".format(str))
-
