7c5570c6de9f363c7d412009d391dda2e9f989f5
marktipton <6675@holbertonstudent.com>
2023-09-11 15:57:02 -0700

    roman numerals without handling special cases 4, 9, 49, etc

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string == None or type(roman_string) != str:
+        return 0
+    count = 0
+    for c in roman_string:
+        if c == 'I':
+            count += 1
+        elif c == 'V':
+            count += 5
+        elif c == 'X':
+            count += 10
+        elif c == 'L':
+            count += 50
+        elif c == 'C':
+            count += 100
+        elif c == 'D':
+            count += 500
+        elif c == 'M':
+            count += 1000
+    return count
+        
+
