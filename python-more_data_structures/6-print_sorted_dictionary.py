07fe4b2722abf594aad9e0cf2b4c6abbe02ba9c7
marktipton <6675@holbertonstudent.com>
2023-09-11 13:19:36 -0700

    a_dictionary.keys() to get keys and my_keys.sort() to sort alphabetically

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    my_keys = list(a_dictionary.keys())
+    my_keys.sort()
+    sorted_dict = {i: a_dictionary[i] for i in my_keys}
+
+    for i in sorted_dict:
+        print(f"{i}: {a_dictionary[i]}")
