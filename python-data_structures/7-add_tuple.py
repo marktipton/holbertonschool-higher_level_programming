88a27f18d66c1932097eea68efa262fe804e1b57
marktipton <6675@holbertonstudent.com>
2023-09-11 16:16:26 -0700

    tuple_a = (tuple_a[0], 0) for len tuple_a == 1

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1 = 0
-    b1 = 0
-    a2 = 0
-    b2 = 0
-    if tuple_a[0]:
-        a1 = tuple_a[0]
-    if tuple_a[1]:
-        a2 = tuple_a[1]
-    if tuple_b[0]:
-        b1 = tuple_b[0]
-    if tuple_b[1]:
-        b2 = tuple_b[1]
+    if len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    if len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
 
+    a1 = tuple_a[0]
+    a2 = tuple_a[1]
+    b1 = tuple_b[0]
+    b2 = tuple_b[1]
     sum1 = a1 + b1
     sum2 = a2 + b2
 
