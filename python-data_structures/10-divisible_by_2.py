432fd648e63a1b6b17bd95620e4ae036b4498827
marktipton <6675@holbertonstudent.com>
2023-09-11 16:44:57 -0700

    pycodestyle

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     new_list = my_list.copy()
     for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0: 
+        if my_list[i] % 2 == 0:
             new_list[i] = True
         else:
             new_list[i] = False
