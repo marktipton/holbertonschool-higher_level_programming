ab453d1f7aaa24a088aa80a21683d2ef8972f46c
marktipton <6675@holbertonstudent.com>
2023-09-10 20:05:46 -0700

    return (a, tuple)

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = 0
+    b1 = 0
+    a2 = 0
+    b2 = 0
+    if tuple_a[0]:
+        a1 = tuple_a[0]
+    if tuple_a[1]:
+        a2 = tuple_a[1]
+    if tuple_b[0]:
+        b1 = tuple_b[0]
+    if tuple_b[1]:
+        b2 = tuple_b[1]
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+
+    return (sum1, sum2)
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return None
+    length = len(sentence)
+    char = sentence[0]
+    return (length, char)
