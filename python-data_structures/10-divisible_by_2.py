0b85d057da70680bbc8d66257ecf5d8059d9cb07
marktipton <6675@holbertonstudent.com>
2023-09-10 21:31:12 -0700

    value for value in my_list if value != element removes an element from a list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = ""
-    for i in range(0, len(my_list)):
-        if 
-        new_list[i] = True
-        
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0: 
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
