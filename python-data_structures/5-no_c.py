e89f614fefda6873a4cd57e5b574e9b1bb46287a
marktipton <6675@holbertonstudent.com>
2023-09-11 16:34:23 -0700

    using double quotesinstead of single

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for x in range(len(my_string)):
-        if my_string[x] == 'c' or my_string[x] == 'C':
+        if my_string[x] == "c" or my_string[x] == "C":
             continue
         new_string += my_string[x]
     return new_string
