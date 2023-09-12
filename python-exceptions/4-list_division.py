6e8ecc775d3cd17ebaaeb4971f979ec57884c593
marktipton <6675@holbertonstudent.com>
2023-09-12 14:23:49 -0700

    pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     my_list_div = []
-    
     for i in range(0, list_length):
         try:
             quotient = my_list_1[i] / my_list_2[i]
@@ -14,6 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             quotient = 0
-        finally: 
+        finally:
             my_list_div.append(quotient)
     return my_list_div
