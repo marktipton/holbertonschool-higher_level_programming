42d3909bb395aa729759a553062b25cee413cd75
marktipton <6675@holbertonstudent.com>
2023-09-12 13:07:08 -0700

    combination of 0 and 1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    int_printed = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            int_printed += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return int_printed
