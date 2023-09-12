d27dc7368df7450f3b21ae42f59c5329673d021b
marktipton <6675@holbertonstudent.com>
2023-09-12 13:18:58 -0700

    set c = None inside of the the except statement

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        c = 0
+        c = a / b
+    except (ZeroDivisionError, TypeError):
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+        return c
