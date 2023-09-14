a92c8e92760f68f81756781d793d1e64e60fb126
marktipton <6675@holbertonstudent.com>
2023-09-14 16:04:37 -0700

    took out another space

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """class square with private attribute size"""
-    
     def __init__(self, size):
         """Args: size (int): The size of the square."""
         self.__size = size
