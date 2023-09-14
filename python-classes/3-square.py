b7816af55b7a5fdebbf6dda69e74f79db037ea80
marktipton <6675@holbertonstudent.com>
2023-09-14 16:49:28 -0700

    need to use self.__size to access the input size....just size does not work because it is not accessible outside of the init method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""defines class square"""
+
+
+class Square:
+    """class square with private attribute size"""
+    def __init__(self, size=0):
+        """Args: size (int): The size of the square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
