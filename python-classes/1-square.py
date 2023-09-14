5c677cbfe0b4ef4a45abda9a5cd35b329120ebee
marktipton <6675@holbertonstudent.com>
2023-09-14 15:51:32 -0700

    added spaes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,11 +4,13 @@
 
 class Square:
     """class square with private attribute size"""
+    
     def __init__(self, size):
+        
         """
         Initializes new Square instance
         Args:
             size (int): The size of the square.
-
         """
+        
         self.___size = size
