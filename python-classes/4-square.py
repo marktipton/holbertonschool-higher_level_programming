47b5b47573d3db865e840394d4ad1d09227cf6f6
marktipton <6675@holbertonstudent.com>
2023-09-14 17:42:57 -0700

    needed to change size to value in the setter method

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,20 +7,23 @@ class Square:
     def __init__(self, size=0):
         """Args: size (int): The size of the square."""
         self.__size = size
+    
+    @property
+    def size(self):
+        """gets size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set a new value for size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """returns current square area"""
         return self.__size ** 2
 
-    def size(self):
-        """gets size of square"""
-        return self.__size
-
-    def size(self, value):
-        """set a new value for size of square"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
