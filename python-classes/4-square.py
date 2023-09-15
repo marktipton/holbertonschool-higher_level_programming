177c0496e34550ed945c347ec465bfa83ee1a3b0
marktipton <6675@holbertonstudent.com>
2023-09-14 17:23:31 -0700

    reaching something wrong in memory but set up getter and setter and moved error logic into setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""defines class square"""
+
+
+class Square:
+    """class square with private attribute size"""
+    def __init__(self, size=0):
+        """Args: size (int): The size of the square."""
+        self.__size = size
+
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
+
+    def size(self):
+        """gets size of square"""
+        return self.__size
+
+    def size(self, value):
+        """set a new value for size of square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
