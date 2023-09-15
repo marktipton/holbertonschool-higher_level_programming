c751c19c0e6e24c07fb2ed349b7b4d12a20d6efd
marktipton <6675@holbertonstudent.com>
2023-09-15 09:32:32 -0700

    made print method for square class. Note: can use multiplication in print statements in python

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""defines class square"""
+
+
+class Square:
+    """class square with private attribute size"""
+    def __init__(self, size=0):
+        """Args: size (int): The size of the square."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """gets size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set a new value for size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints square with # in stdout"""
+        if self.__size == 0:
+            print()
+        for i in range (self.__size + 1):
+            print("#" * self.__size)
