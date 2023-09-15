5e9d94ef26734196c03152959f07470d954a122d
marktipton <6675@holbertonstudent.com>
2023-09-14 17:44:18 -0700

    pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """Args: size (int): The size of the square."""
         self.__size = size
-    
+
     @property
     def size(self):
         """gets size of square"""
@@ -26,4 +26,3 @@ class Square:
     def area(self):
         """returns current square area"""
         return self.__size ** 2
-
