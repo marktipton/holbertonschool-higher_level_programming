3d82677a4a9fc233116f92d24fb8b032ca81885c
marktipton <6675@holbertonstudent.com>
2023-09-18 13:13:59 -0700

    cast a and be to int

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,10 +3,13 @@
 adds two integers and returns sum
 """
 
+
 def add_integer(a, b=98):
     """adds integers a and b"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an intger")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
     return a + b
