f6244a259cb03fdb93c38af27591c92cba94e536
marktipton <6675@holbertonstudent.com>
2023-09-18 14:03:30 -0700

    set up matrix test file and wrote initial script for divided matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+divides every element of a matrix by a specified value
+"""
+
+
+def matrix_divided(matrix, div):
+    """Divides elements of matrix by value div"""
+    divided_matrix = [[]]
+    for row in matrix:
+        divided_row = []
+        for element in row:
+            divided_row.append(element / div)
+        divided_matrix.append(divided_row)
+    return divided_matrix
