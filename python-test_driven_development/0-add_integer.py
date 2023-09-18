3e75900f6467308664d29c1718f6bfacf8636c76
marktipton <6675@holbertonstudent.com>
2023-09-18 13:00:36 -0700

    made initial tests and code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+adds two integers and returns sum
+"""
+
+def add_integer(a, b=98):
+    """adds integers a and b"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an intger")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return a + b
