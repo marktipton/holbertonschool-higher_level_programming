8e0d91742379daedce530e5ef6454e3340f89932
marktipton <6675@holbertonstudent.com>
2023-09-18 15:09:03 -0700

    passed tests for div zero and div NaN

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,6 +7,10 @@ divides every element of a matrix by a specified value
 def matrix_divided(matrix, div):
     """Divides elements of matrix by value div"""
     divided_matrix = [[]]
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
     for row in matrix:
         divided_row = []
         for element in row:
