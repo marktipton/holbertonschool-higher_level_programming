538646ddab705a3abbd9cf204b4147f8fbbea2ea
marktipton <6675@holbertonstudent.com>
2023-09-19 12:08:30 -0700

    fixed typo

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ adds two integers and returns sum
 def add_integer(a, b=98):
     """adds integers a and b"""
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an intger")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
