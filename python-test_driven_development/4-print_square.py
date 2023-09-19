3adda366f68495d12d781cc54865b0f2b488e0c1
marktipton <6675@holbertonstudent.com>
2023-09-18 17:55:49 -0700

    test size must be an integer

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""prints a square using the '#' character"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
