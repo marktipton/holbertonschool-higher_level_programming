b29555c8e97c6dbe9cbc15b8ce111bf9765bdde2
marktipton <6675@holbertonstudent.com>
2023-09-18 18:07:33 -0700

    partially through print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,5 +5,7 @@
 def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must
     for i in range(size):
         print("#" * size)
