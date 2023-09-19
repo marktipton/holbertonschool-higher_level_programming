93a1e8cdb1c2d135dab0961224052ea1e20ca5b0
marktipton <6675@holbertonstudent.com>
2023-09-18 17:23:51 -0700

    raise typeerror if first or last name is not string

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""prints first name and last name input"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints first and last name input if they are strings"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
