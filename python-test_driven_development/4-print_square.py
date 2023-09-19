5c1024aa51616dfb82540dcaeca00a2b0872899e
marktipton <6675@holbertonstudent.com>
2023-09-18 20:10:20 -0700

    typo

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,6 +7,6 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise TypeError("size must >= 0")
+        raise TypeError("size must be >= 0")
     for i in range(size):
         print("#" * size)
