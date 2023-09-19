ea98cdc4611669074221769731670fa165cb52c8
marktipton <6675@holbertonstudent.com>
2023-09-18 20:24:35 -0700

    initial print text function

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""prints text with 2 new lines after '.', '?', and ':'"""
+
+
+def text_indentation(text):
+    """iterates through text string and puts lines at specials chars"""
+    for element in text:
+        if element == '.' or element == ':' or element == '?':
+            print(element)
+            print()
+        else:
+            print(element, end='')
