d178295bdd252469946c6086b74c760fa87dfbe8
marktipton <6675@holbertonstudent.com>
2023-09-18 21:12:51 -0700

    added tests for middle, end, one element, and empty

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,3 +10,16 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_negative(self):
         self.assertEqual(max_integer([-3, -7, -9]), -3)
+
+    def test_middle_max(self):
+        self.assertEqual(max_integer([4, 30, 15]), 30)
+
+    def test_end_max(self):
+        test_max = max_integer([15, 20, 25])
+        self.assertEqual(test_max, 25, "should return max if el at end")
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def list_empty(self):
+        self.assertIsNone(max_integer([]))
