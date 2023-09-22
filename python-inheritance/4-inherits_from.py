2267dc59a9378deb1902f175ecb116db72755279
marktipton <6675@holbertonstudent.com>
2023-09-22 14:03:26 -0700

    obj is instance of subclass if isinstance but not type(obj) == a_class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""returns true obj is instance of a subclass of a class"""
+
+
 def inherits_from(obj, a_class):
+    """checks if obj is instance of subclass of a_class"""
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
