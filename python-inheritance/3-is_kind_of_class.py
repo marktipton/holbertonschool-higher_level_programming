6dcfe16e1c7f703662eb6a3ebc8b63126b3303fc
marktipton <6675@holbertonstudent.com>
2023-09-22 13:56:21 -0700

    now use isinstance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+"""returns true if isinstance of of object or subclass of object"""
+
+
 def is_kind_of_class(obj, a_class):
+    """true if object or subclass of object"""
+    return isinstance(obj, a_class)
