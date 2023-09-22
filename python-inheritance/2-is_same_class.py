a24842335290833c89032fe7b3c0c1e30d260cd0
marktipton <6675@holbertonstudent.com>
2023-09-22 13:51:16 -0700

    isinstance and type() == are different in the way that they handle inheritance....subclasses are not considered by type()

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,6 +4,6 @@
 
 def is_same_class(obj, a_class):
     """returns true if object is exactly and instance of class"""
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
