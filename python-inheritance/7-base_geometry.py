1618e312878aeb587b22347dd2ae4ab36b137f3e
marktipton <6675@holbertonstudent.com>
2023-09-22 14:23:07 -0700

    no test yet but made typeerror and valueerror statements

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""class base geometry"""
+
+
+class BaseGeometry:
+    """class geometry with method to find area"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
