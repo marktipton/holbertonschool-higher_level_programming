407c96af8f3dc36b102b4d784ec5704cf61c9a58
marktipton <6675@holbertonstudent.com>
2023-09-22 12:47:11 -0700

    super().__init__() to inherit parent information?!?

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-def print_sorted(self):
+"""prints list in ascending order"""
+
+
+class MyList(list):
+    """child class MyList with parent list"""
+    def __init__(self):
+        super().__init__()
+
+
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
