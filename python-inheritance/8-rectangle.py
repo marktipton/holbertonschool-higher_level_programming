c915b419804cad9a23e5a1fd20374251dccbb0c5
marktipton <6675@holbertonstudent.com>
2023-09-22 14:43:35 -0700

    make width and height dependent on integer_validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
-        self.__width = width
-        self.__heiht = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
