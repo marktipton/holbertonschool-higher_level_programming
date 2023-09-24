73612c9f43788291b2860bf49f9fee2a0462ea36
marktipton <6675@holbertonstudent.com>
2023-09-24 12:26:30 -0700

    append text to file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""""appends text to a file"""
+
 
 def append_write(filename="", text=""):
+    """appends text to a file"""
+    with open(filename, 'a', encoding='utf-8') as file:
+        chars_appended = file.write(text)
+        return chars_appended
