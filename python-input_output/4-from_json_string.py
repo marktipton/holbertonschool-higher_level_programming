785d4bc59809f3fd8bc994896af4cba33566357a
marktipton <6675@holbertonstudent.com>
2023-09-24 12:57:49 -0700

    convert from json to object using json.loads(json_string)

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""converts from json to object"""
+import json
+
 
 def from_json_string(my_str):
+    """converts from json to object"""
+    my_obj = json.loads(my_str)
+    return my_obj
