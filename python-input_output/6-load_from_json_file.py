d62c4ba5df2a0dc3015c6cac90159e2cc749bc5a
marktipton <6675@holbertonstudent.com>
2023-09-24 13:25:33 -0700

    load mobject from json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""loads object from json file"""
+import json
+
 
 def load_from_json_file(filename):
+    """loads object from json file"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        my_obj = json.loads(file.read())
+        return my_obj
