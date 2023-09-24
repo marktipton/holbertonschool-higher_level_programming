df3015181c7d98c7fbcf994053433f3d75b43ac4
marktipton <6675@holbertonstudent.com>
2023-09-24 12:17:30 -0700

    write to file and count characters written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""writes a string to a utf8 text file"""
+
 
 def write_file(filename="", text=""):
+    """writes string to text file"""
+    with open(filename, 'w', encoding = 'utf-8') as file:
+        num_chars = file.write(text)
+        return num_chars
