7fe0f82aabe68d01c799d6f133424f3aa8591397
marktipton <6675@holbertonstudent.com>
2023-09-24 13:13:23 -0700

    use json.dump(my_obj, f) to write json object rep to file f

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""writes json represenation of object to a file"""
+import json
+
 
 def save_to_json_file(my_obj, filename):
+    """writes json representation of object to a file"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json_str = json.dump(my_obj, file)
+        return json_str
