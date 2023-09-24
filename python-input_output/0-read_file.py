b8f66bd70edb68457264059fa53c0cca0bb1c4a2
marktipton <6675@holbertonstudent.com>
2023-09-24 11:29:57 -0700

    with open(filename, more, encoding) as file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""reads a text file and prints it to stdout"""
+
 
 def read_file(filename=""):
+    """reads file and prints it"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
