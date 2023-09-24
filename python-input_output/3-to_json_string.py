264c8e05a6237cf559716f1b20a5e958348953e3
marktipton <6675@holbertonstudent.com>
2023-09-24 12:45:52 -0700

    pycodestyle

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,9 +2,8 @@
 """returns the JSON representation of an object"""
 import json
 
+
 def to_json_string(my_obj):
     """returns the JSON representation of an object"""
     json_string = json.dumps(my_obj)
     print(json_string)
-
-
