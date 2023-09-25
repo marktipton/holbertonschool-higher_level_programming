f9fbd573c12a7c7ba8516e70554a697b0ee41bcd
marktipton <6675@holbertonstudent.com>
2023-09-25 09:43:20 -0700

    use hasattr and getattr

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student:
+    """class student with attr for firstname, lastname, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes public attributes of class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary description of self"""
+        if attrs is None:
+            return vars(self)
+
+        dictionary_des = {}
+        for attr_name in attrs:
+            if hasattr(self, attr_name):
+                dictionary_des[attr_name] = getattr(self, attr_name)
+        return dictionary_des
