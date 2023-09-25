be0e0f5aeb25f6afde5ee9ba6ebc3cb87a525895
marktipton <6675@holbertonstudent.com>
2023-09-25 15:26:23 -0700

    added docstrings and more tests. changed error ststements to .format

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,11 +4,14 @@
 
 class BaseGeometry:
     """class geometry with method to find area"""
+    
     def area(self):
+        """gives exception if not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """checks if integer input is greater than 0"""
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
