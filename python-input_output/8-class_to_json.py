25153c69531780c2183aceb01d9ef3914c6ddc10
marktipton <6675@holbertonstudent.com>
2023-09-25 08:51:06 -0700

    no longer including class name

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,8 +4,11 @@
 
 def class_to_json(obj):
     """returns dictionary description of an object"""
+    """
     class_name = obj.__class__.__name__
     dictionary_des = {"__class__": class_name}
+    """
+    dictionary_des = {}
     attributes = vars(obj)
     for at_name, at_value in attributes.items():
         dictionary_des[at_name] = at_value
