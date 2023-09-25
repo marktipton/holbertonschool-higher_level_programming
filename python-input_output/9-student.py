44e68c0d507bef8b63a731827ef6576316d15683
marktipton <6675@holbertonstudent.com>
2023-09-25 09:15:46 -0700

    class student with method to retrieve dicgtionary

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+"""defines class Student"""
+
 
 class Student:
+    """class student with attr for firstname, lastname, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes public attributes of class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dictionary description of self"""
+        dictionary_des = {}
+        attr = vars(self)
+        for attr_name, attr_value in attr.items():
+           dictionary_des[attr_name] = attr_value
+        return dictionary_des
