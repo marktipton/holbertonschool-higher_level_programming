115b0e2395af7eb0c36c49176715d81b875d89a7
marktipton <6675@holbertonstudent.com>
2023-09-26 12:22:43 -0700

    initial load add save script

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student:
+    """class student with attr for firstname, lastname, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes public attributes of class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary description of self"""
+        if attrs is None:
+            return vars(self)
+
+        dictionary_des = {}
+        for attr_name in attrs:
+            if hasattr(self, attr_name):
+                dictionary_des[attr_name] = getattr(self, attr_name)
+        return dictionary_des
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the student instance"""
+        for first_name, last_name, age in json.items():
+            setattr(self, first_name, last_name, age)
