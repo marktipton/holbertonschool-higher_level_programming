9e760ca2e119f2a1b13b27ca3bc40ada14dcc8f9
marktipton <6675@holbertonstudent.com>
2023-09-26 12:28:52 -0700

    import sys and import json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """adds all arguments to a python list"""
 
-from sys import argv
+import sys
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').save_to_json_file
 
