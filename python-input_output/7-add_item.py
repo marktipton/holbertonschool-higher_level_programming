bf7573d5c584c3243f148650a0ab9bb3ccbc5896
marktipton <6675@holbertonstudent.com>
2023-09-26 12:27:00 -0700

    took out if __name__ = __main__

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-    try:
-        items = load_from_json_file("add_item.json")
+try:
+    items = load_from_json_file("add_item.json")
 
-    except FileNotFoundError:
-        items = []
+except FileNotFoundError:
+    items = []
 
-    items.extend(sys.argv[1:])
+items.extend(sys.argv[1:])
 
-    save_to_json_file(items, "add_item.json")
+save_to_json_file(items, "add_item.json")
