f9d9b4dc6c2b6b6bfd216dcbe9e314c9e1c726a5
marktipton <6675@holbertonstudent.com>
2023-09-25 19:13:56 -0700

    took out if statements

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""creates rectangle child class of BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class child of basegeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        """unofficial string representation of rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
