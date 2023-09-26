0289379082cdfac78b3949462adbadd2bbb40676
marktipton <6675@holbertonstudent.com>
2023-09-26 12:24:33 -0700

    imported argv from sys

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """adds all arguments to a python list"""
 
-
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').save_to_json_file
 
