fcd625f7a9d801185ef75f7f90b2daf6a6ade8ec
marktipton <6675@holbertonstudent.com>
2023-09-26 11:00:46 -0700

    used super to refer to superclass rectangle

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""creates rectangle child class of BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """rectangle class child of basegeometry"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
