17817c3e901bdd1c7fd1d10f2df4b247dffb9cfc
marktipton <6675@holbertonstudent.com>
2023-09-25 17:33:26 -0700

    added docstring

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """rectangle class child of basegeometry"""
     def __init__(self, width, height):
         if self.integer_validator("width", width):
             self.__width = width
