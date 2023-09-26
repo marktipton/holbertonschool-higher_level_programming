de55c54a8b766dc113190f30c4591106ff95a79a
marktipton <6675@holbertonstudent.com>
2023-09-26 14:06:44 -0700

    nested loops to make pascals triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""returns pascals triangle using a list of lists"""
+
 
 def pascal_triangle(n):
+    """returns pascals triangle using a list of lists"""
+    lol = []
+    if n <= 0:
+        return lol
+    lol = [[1] for x in range(n)]
+
+    for x in range(1, n):
+        for y in range(1, x):
+            lol[x].append(lol[x - 1][y - 1] + lol[x - 1][y])
+        lol[x].append(1)
+
+    return lol
