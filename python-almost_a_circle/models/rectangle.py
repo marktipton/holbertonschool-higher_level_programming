42ae3ac16bf393d587655da2766d0065103c5a2e
marktipton <6675@holbertonstudents.com>
2023-09-29 09:53:26 -0700

    added getters and setters to rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Creates rectangle class which inherits from base"""
 
+from models.base import Base
+
 
 class Rectangle(Base):
     """rectangle class child of base"""
@@ -10,3 +12,35 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
