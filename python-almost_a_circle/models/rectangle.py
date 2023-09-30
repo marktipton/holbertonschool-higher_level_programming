fe9094b835e7c5e3c82da407bb5ef7112bbe2c9e
marktipton <6675@holbertonstudents.com>
2023-09-30 10:14:19 -0700

    assign args to attributes starting with self.id = args[0]

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,11 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
             f"{self.width}/{self.height}"
         )
+    
+    def update(self, *args):
+        """assigns an arg to each rectangle attribute"""
+        self.id = args[0]
+        self.width = args[1]
+        self.height = args[2]
+        self.x = args[3]
+        self.y = args[4]
