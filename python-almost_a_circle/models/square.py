6319936cb0bf60b67fb58bd5380a4bf9e173f004
marktipton <6675@holbertonstudents.com>
2023-09-30 15:22:27 -0700

    just passing size instead of width=size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,4 +7,4 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """square class child of rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x, y, id)
+        super().__init__(size, size, x, y, id)
