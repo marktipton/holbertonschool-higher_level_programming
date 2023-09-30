4f634c7e6905691c2727c425ac0dcfd9be77f986
marktipton <6675@holbertonstudents.com>
2023-09-30 09:52:38 -0700

    trying different way to split f string into two lines

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,5 +72,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """return string rep of rectangle"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
-             - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
+        f"{self.width}/{self.height}"
