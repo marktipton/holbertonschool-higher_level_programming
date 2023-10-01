0ce81b184d01fe90ab2d1f23f5a5e0f3f805302b
marktipton <6675@holbertonstudents.com>
2023-10-01 11:42:28 -0700

    added __str__ representation fro square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,3 +8,10 @@ class Square(Rectangle):
     """square class child of rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+       """return string rep of rectangle"""
+       return (
+           f"[Square] ({self.id}) {self.x}/{self.y} - "
+           f"{self.width}"
+        )
