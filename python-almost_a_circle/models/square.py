0f13ce935247b4d2bb5437c938a60f82b21832d3
marktipton <6675@holbertonstudents.com>
2023-10-01 12:02:14 -0700

    moved getter and setter above str method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,13 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """return string rep of rectangle"""
-        return (
-           f"[Square] ({self.id}) {self.x}/{self.y} - "
-           f"{self.width}"
-        )
-
     @propery 
     def size(self):
         """gets size attribute of square"""
@@ -26,3 +19,10 @@ class Square(Rectangle):
         """sets width and height to same size"""
         self.width = value
         self.height = value
+
+    def __str__(self):
+        """return string rep of rectangle"""
+        return (
+           f"[Square] ({self.id}) {self.x}/{self.y} - "
+           f"{self.width}"
+        )
