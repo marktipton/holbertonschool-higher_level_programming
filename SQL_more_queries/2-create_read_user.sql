4ab0f0ab0b943b6baac316fdc4c5bddcb6c5c202
mrktptn <marktipton95@gmail.com>
2023-10-20 16:00:55 -0500

    hbtn_0d_2 not in quotes

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates user user_0d_2 and sets password and gives select priveleges
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT READ ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
