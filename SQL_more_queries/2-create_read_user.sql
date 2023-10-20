107a9232cf84856fb8e15c252a151d61ded6bca8
mrktptn <marktipton95@gmail.com>
2023-10-20 15:59:38 -0500

    grant read

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates user user_0d_2 and sets password and gives select priveleges
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+GRANT READ ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 FLUSH PRIVILEGES;
