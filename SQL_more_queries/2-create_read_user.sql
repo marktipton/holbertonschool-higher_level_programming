22e5a9b4de241fb4d09ac293cd09e5022554c4be
mrktptn <marktipton95@gmail.com>
2023-10-20 15:55:51 -0500

    @localhost instead of database name but grant priveleges on databasename

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creates user user_0d_2 and sets password and gives select priveleges
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
