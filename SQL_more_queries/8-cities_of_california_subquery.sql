6e5231661086cd413b12fd2aa06f96245ba5a66b
mrktptn <marktipton95@gmail.com>
2023-10-22 13:18:22 -0500

    select * from cities instead of cities.* from cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 USE hbtn_0d_usa;
-SET @California_id = (SELECT id FROM states WHERE name = 'California');
+SET @California_state_id = (SELECT id FROM states WHERE name = 'California');
 
-SELECT cities.* FROM cities
-WHERE state_id = @California_id
+SELECT * FROM cities
+WHERE state_id = @California_state_id
 ORDER BY cities.id;
