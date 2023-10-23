bd91146b1274a626f3970553a4a7b4337b265bfe
mrktptn <marktipton95@gmail.com>
2023-10-22 19:25:15 -0500

    simplify by using id directly

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,3 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 USE hbtn_0d_usa;
-SET @California_state_id = (SELECT id FROM states WHERE name = 'California');
-
-SELECT id, name FROM cities
-WHERE state_id = @California_state_id
-ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY cities.id;
