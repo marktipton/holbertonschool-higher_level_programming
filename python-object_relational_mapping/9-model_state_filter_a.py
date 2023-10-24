590e8144e80a9905a3c624e725ad719c3cf4358f
mrktptn <marktipton95@gmail.com>
2023-10-24 16:51:01 -0500

    .filter is the equivalent of WHERE

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states(username, password, database_name):
+    """list states in hbtn_0e_0_usa database"""
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State)\
+                    .filter(State.name.like('%a%'))\
+                    .order_by(State.id)
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    list_states(username, password, database_name)
