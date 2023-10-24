2aa63db3a3fe2e80bf4eab8a24bca4bee16bac36
mrktptn <marktipton95@gmail.com>
2023-10-24 16:22:00 -0500

    there is a .first() option...of course there is!

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states(username, password, database_name):
+    """list states in hbtn_0e_0_usa database"""
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    first_states = session.query(State).order_by(State.id).first()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    list_states(username, password, database_name)
