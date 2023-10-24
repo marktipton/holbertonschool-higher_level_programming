54b897658774d5f0aebf6229ed05b6d4c2298ec0
mrktptn <marktipton95@gmail.com>
2023-10-24 15:14:14 -0500

    list for state input

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Lists all cities from database hbtn_0e_4_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+def list_cities(username, password, database_name, state_name):
+    """Lists all cities from the database hbtn0e_4_usa"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+    # Execute SQL Query
+    cursor.execute("""
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name LIKE BINARY '{}'
+        ORDER BY cities.id
+    """.format(state_name))
+
+    results = cursor.fetchall()
+
+    for city in results:
+        print(city)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    list_cities(username, password, database_name, state_name)
