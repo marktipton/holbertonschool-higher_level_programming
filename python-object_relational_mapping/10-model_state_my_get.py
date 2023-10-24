8bbaf3a722045e6a00f9661b40c5c7aefb58df64
mrktptn <marktipton95@gmail.com>
2023-10-24 16:56:49 -0500

    alchemy way to check if state matches input

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states(username, password, database_name, state_searched):
+    """list states in hbtn_0e_0_usa database"""
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State)\
+                    .filter(State.name == state_searched)\
+                    .order_by(State.id)
+
+    if state:
+        print(state.id)
+    else:
+        print("Not Found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_searched = sys.argv[4]
+    list_states(username, password, database_name, state_searched)
