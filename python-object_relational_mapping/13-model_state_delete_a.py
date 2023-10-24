26f0f1520479dac580f009d5c9de2ca37448de42
mrktptn <marktipton95@gmail.com>
2023-10-24 17:28:29 -0500

    delete states starting with a

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states(username, password, database_name):
+    """list states in hbtn_0e_0_usa database"""
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states_delete = session.query(State)\
+                    .filter(State.name.like('%a%)).all()
+
+    for state in states_delete:
+        session.delete(state)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    list_states(username, password, database_name)
