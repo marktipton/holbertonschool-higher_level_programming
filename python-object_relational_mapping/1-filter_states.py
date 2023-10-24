23fd911e620d0ebcd520e81bb6af65c4ebbde3db
mrktptn <marktipton95@gmail.com>
2023-10-23 22:05:37 -0500

    list states that start with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""
-lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa
-"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
 
 import MySQLdb
 import sys
 
-def list_states(username, password, database_name):
-    """states starting with N"""
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password,
+def list_states_N(username, password, database_name, host, port):
+    """list states in hbtn_0e_0_usa database"""
+    db = MySQLdb.connect(host=host, port=port, user=username, passwd=password,
     db=database_name)
 
     cursor = db.cursor()
     # Execute SQL Query
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor.execute("""
+        SELECT *
+        FROM states
+        WHERE name LIKE 'N%'
+        ORDER BY states.id
+    """)
 
     results = cursor.fetchall()
 
@@ -31,4 +33,4 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
     host = 'localhost'
     port = 3306
-    list_states(username, password, database_name)
+    list_states(username, password, database_name, host, port)
