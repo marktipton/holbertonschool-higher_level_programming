b7aff52c9799ac47cfccda2feee7cc2754435535
mrktptn <marktipton95@gmail.com>
2023-10-24 17:56:07 -0500

    import model_state into model_city

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ connect to database using sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from model_state import Base, State
 
 Base = declarative_base()
 
