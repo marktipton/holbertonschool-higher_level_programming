cd43cbe1d70e27385e389eeccf357bdd5a0ac677
mrktptn <marktipton95@gmail.com>
2023-11-22 13:31:06 -0600

    javascript needs 2 spaces for tabs! used process.argv.length to handle arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const numArgs = process.argv.length - 2;
+
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument');
+} else {
+  console.log('Arguments');
+}
