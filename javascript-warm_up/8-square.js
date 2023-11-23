22711dea9aeb97f01b667b2dd545c8b69f409502
mrktptn <marktipton95@gmail.com>
2023-11-23 11:22:00 -0600

    semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,5 +3,5 @@ squareSize = process.argv[2];
 if (!parseInt(squareSize)) console.log('Missing size');
 
 for (let i = 0; i < squareSize; i++) {
-  console.log('X'.repeat(squareSize))
+  console.log('X'.repeat(squareSize));
 }
