dd12fe7e57a41f5aeb2247a25aad129b6cfc0dce
mrktptn <marktipton95@gmail.com>
2023-11-23 11:33:10 -0600

    must use parseInt to make sure that the input is an integer before performing addition

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+function add (a, b) {
+  return a + b;
+}
+const result = add(a, b);
+console.log(result);
