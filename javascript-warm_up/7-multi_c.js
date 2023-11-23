4cb1011f326c509cc1484d0a21fc73779e8ddac7
mrktptn <marktipton95@gmail.com>
2023-11-23 11:13:24 -0600

    missing number of occurences or print by argv number input

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const C = 'C is fun';
+const x = process.argv[2];
+if (!parseInt(x)) console.log('Missing number of occurences');
+
+for (let i = 0; i < x; i++) {
+  console.log(C);
+}
