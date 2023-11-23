6b82e816f37e7bc43baee7ea580cacf82735d240
mrktptn <marktipton95@gmail.com>
2023-11-23 11:21:08 -0600

    using .repeat instead of nested loops

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+squareSize = process.argv[2];
+if (!parseInt(squareSize)) console.log('Missing size');
+
+for (let i = 0; i < squareSize; i++) {
+  console.log('X'.repeat(squareSize))
+}
