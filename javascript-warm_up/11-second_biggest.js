fb4c93135a9ec9b30d61f997f9d89c97496a9189
mrktptn <marktipton95@gmail.com>
2023-11-24 12:00:28 -0600

    use loop to find second greatest integer in args array. Need to be careful to use parseInt in the right place or comparison doesn't work correctly

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const args = process.argv;
+// console.log(args.length);
+if (!args[4]) {
+  console.log(0);
+} else {
+  let greatest = null;
+  let second = null;
+  for (let i = 2; i < args.length; i++) {
+    // parseInt(args[i]);
+    // console.log(args[i]);
+    if (args[i] > greatest) {
+      second = parseInt(greatest);
+      greatest = parseInt(args[i]);
+      // console.log(second, greatest);
+    }
+    // } else if (args[i] > second) {
+    //   second = args[i];
+    // }
+  }
+  console.log(second);
+  // console.log(greatest);
+}
