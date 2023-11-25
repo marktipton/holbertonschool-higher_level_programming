ccb1ad0b4ae46b18f2c972b3d3f970106ec06455
mrktptn <marktipton95@gmail.com>
2023-11-24 22:24:28 -0600

    added charPrint method

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square.js');
+
+class Square extends Rectangle {
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
