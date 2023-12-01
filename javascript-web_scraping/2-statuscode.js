6a0e90b361f53b013d400426c3faefc1be573d91
mrktptn <marktipton95@gmail.com>
2023-12-01 17:30:23 -0600

    using request module to get status code from url

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
