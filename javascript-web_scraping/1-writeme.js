cf6cc132d3140218d538a38c0e461b24c496ac94
mrktptn <marktipton95@gmail.com>
2023-12-01 17:05:06 -0600

    write to file using fs.writeFile

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const args = process.argv;
+const fileName = args[2];
+const writeString = args[3];
+
+fs.writeFile(fileName, writeString, 'utf-8', (err) => {
+  if (err) throw err;
+})
