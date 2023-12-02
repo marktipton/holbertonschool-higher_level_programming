8eab2417a6e89229097987aa3d8456753298e8b0
mrktptn <marktipton95@gmail.com>
2023-12-02 12:16:17 -0600

    rough outline for counting wedge antilles appearances

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const wedgeUrl = `https://swapi-api.hbtn.io/api/people/18/`;
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, request, body) => {
+  if (error) {
+    console.error('Error', error.message);
+    process.exit(1);
+  } else {
+    const filmData = JSON.parse(body);
+    const wedgeAppearances = filmData.characters.
+  }
+});
