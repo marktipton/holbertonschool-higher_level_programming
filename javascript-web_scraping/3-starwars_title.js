242b8d7d5a1f5a6c94c18170a715a006eee3e433
mrktptn <marktipton95@gmail.com>
2023-12-02 11:36:24 -0600

    using star wars api to get film title with film id number input

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const filmId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  } else {
+    const filmData = JSON.parse(body);
+    console.log(`${filmData.title}`);
+  }
+});
