ada2b18629b2a42a7e6afb4da2836eb4ae3291c0
mrktptn <marktipton95@gmail.com>
2023-12-02 21:33:54 -0600

    parse task data for users and completed tasks

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+let dictCompleted = {};
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error', error.message);
+    process.exit(1);
+  } else {
+    const taskData = JSON.parse(body);
+    // console.log(taskData);
+    for (const index in taskData) {
+      let task = taskData[index];
+      if (task.completed === true) {
+        if (dictCompleted[task.userId] === undefined) {
+          dictCompleted[task.userId] = 1;
+        } else {
+          dictCompleted[task.userId]++;
+        }
+      }
+    }
+    console.log(dictCompleted);
+  }
+});
