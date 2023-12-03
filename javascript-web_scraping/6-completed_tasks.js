70d3494c2c5bd181baf7e8a156aecffb0cde6827
mrktptn <marktipton95@gmail.com>
2023-12-02 21:45:03 -0600

    added some comments

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,6 +2,8 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
+// empty dictionary, keys will be userIds and values will be number of
+// tasks completed
 const dictCompleted = {};
 request(apiUrl, (error, response, body) => {
   if (error) {
@@ -9,13 +11,15 @@ request(apiUrl, (error, response, body) => {
     process.exit(1);
   } else {
     const taskData = JSON.parse(body);
-    // console.log(taskData);
+    // parse JSON from API into JS object
     for (const index in taskData) {
       const task = taskData[index];
       if (task.completed === true) {
         if (dictCompleted[task.userId] === undefined) {
+          // if user not yet defined in dictionary, make count 1
           dictCompleted[task.userId] = 1;
         } else {
+          // add to count
           dictCompleted[task.userId]++;
         }
       }
