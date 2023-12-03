e57f04712b1e6e1a136313047e206b1e0c83b245
mrktptn <marktipton95@gmail.com>
2023-12-02 20:50:15 -0600

    use fs module to write body response to file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const fileStore = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  } else {
+    fs.writeFile(fileStore, body, 'utf-8', (err) => {
+      if (err) throw err;
+    });
+  }
+});
