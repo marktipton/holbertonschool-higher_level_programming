afaa8eecb6e189b9ac0398fb1548e8489469564f
mrktptn <marktipton95@gmail.com>
2023-12-04 22:33:43 -0600

    added film names for each movie by using .each to get each film and append one by one

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+$.ajax({
+  url: apiUrl,
+  method: 'GET',
+  dataType: 'json',
+  success: function(data) {
+    const filmData = data.results;
+    const movieList = $('#list_movies');
+    $.each(filmData, function(index, film) {
+      movieList.append('<li>' + film.title + '</li>');
+    });
+  }
+});
